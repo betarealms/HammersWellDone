2fffe6c2b3109c7f671887d8198558c51eba83fb
realpetrmolek <petr@pmolek.cz>
2023-08-26 14:41:42 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HammersWellDone
+
+Plugin that adds well done, vanilla-friendly 3x3 and 5x5 hammers
---
+++ src/main/java/com/betarealms/hammerswelldone/HammersWellDone.java
@@ -0,0 +1,20 @@
+package com.betarealms.hammerswelldone;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HammersWellDone extends JavaPlugin {
+
+    // On plugin enable
+    @Override
+    public void onEnable() {
+
+        getLogger().info("Enabled");
+    }
+
+    // On plugin disable
+    @Override
+    public void onDisable() {
+
+        getLogger().info("Enabled");
+    }
+}
