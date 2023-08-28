472a3ab4ea20b46b7f29a1a1cfbdfa50ccf8e9bc
realpetrmolek <petr@pmolek.cz>
2023-08-28 18:44:31 +0200

    Changed block breaking logic to respect other plugins

--- src/main/java/com/betarealms/hammerswelldone/events/OnBlockBreak.java
+++ src/main/java/com/betarealms/hammerswelldone/events/OnBlockBreak.java
@@ -5,6 +5,7 @@ import com.betarealms.hammerswelldone.utils.BlockManager;
 import com.betarealms.hammerswelldone.utils.ToolManager;
 import java.util.HashMap;
 import java.util.UUID;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -21,11 +22,14 @@ import org.bukkit.inventory.meta.ItemMeta;
  * This class handles block breaking.
  */
 public class OnBlockBreak implements Listener {
-  // Using a HashMap to store the BlockFace for each player
+  // Using a HashMap to store the BlockFace for each player.
   private final HashMap<UUID, BlockFace> playerBlockFaceMap = new HashMap<>();
 
+  // This is used to keep track whether to stay in the block breaking loop.
+  private final HashMap<UUID, Block> playerBreakingMap = new HashMap<>();
+
   /**
-   * This triggers on player interact.
+   * This triggers on player , for ex. when punching.
    *
    * @param event PlayerInteractEvent
    */
@@ -42,7 +46,7 @@ public class OnBlockBreak implements Listener {
    *
    * @param event BlockBreakEvent
    */
-  @EventHandler(priority = EventPriority.LOW)
+  @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
   public void onBlockBreak(BlockBreakEvent event) {
     // Get player who broke the block
     Player player = event.getPlayer();
@@ -55,6 +59,7 @@ public class OnBlockBreak implements Listener {
       return;
     }
 
+    // Get item meta
     ItemMeta meta = itemInHand.getItemMeta();
 
     // Prevent NullPointerException
@@ -62,24 +67,50 @@ public class OnBlockBreak implements Listener {
       return;
     }
 
-    // Check for customModelData
-    if (meta.hasCustomModelData() && (ToolManager.decodeTier(meta.getCustomModelData())
-        == Tier.ADVANCED || ToolManager.decodeTier(meta.getCustomModelData()) == Tier.GOD)) {
+    // Check if tool isn't from this plugin
+    if (!meta.hasCustomModelData()
+        || (ToolManager.decodeTier(meta.getCustomModelData()) != Tier.ADVANCED
+        && ToolManager.decodeTier(meta.getCustomModelData()) != Tier.GOD)) {
+      return;
+    }
 
-      // Get mined block
-      final Block block = event.getBlock();
+    // Get mined block
+    final Block block = event.getBlock();
+
+    // Is this in a recursive loop?
+    // Does playerBreakingMap contain a key for this player?
+    // Is the block being broken the same as the mined block?
+    if (playerBreakingMap.containsKey(player.getUniqueId())
+        && !block.equals(playerBreakingMap.get(player.getUniqueId()))) {
+      block.breakNaturally(itemInHand);
+      // Todo: DAMAGE
+    } else {
+      // Add the mined block to playerBreakingMap
+      playerBreakingMap.put(player.getUniqueId(), block);
 
       // Get mined blocks face
       final BlockFace blockFace = playerBlockFaceMap.getOrDefault(player.getUniqueId(), null);
 
-      for (Block b : BlockManager.getSurroundingBlocks(
+      // Iterate through all the blocks to remove
+      for (Block blockToRemove : BlockManager.getSurroundingBlocks(
           ToolManager.decodeTier(meta.getCustomModelData()).getBit(), block, blockFace)) {
-        b.breakNaturally();
+        // Check if it can be broken
+        if (canBreakBlock(blockToRemove, itemInHand)) {
+          // Start a new instance of BlockBreakEvent that will break the block
+          BlockBreakEvent e = new BlockBreakEvent(blockToRemove, player);
+          Bukkit.getPluginManager().callEvent(e);
+        }
       }
 
-      // Remove mined block
-      playerBlockFaceMap.remove(player.getUniqueId());
-
+      // Remove the mined block from playerBreakingMap
+      playerBreakingMap.remove(player.getUniqueId());
     }
+
+    // Remove BlockFace as it's not needed anymore
+    playerBlockFaceMap.remove(player.getUniqueId());
+  }
+
+  private static boolean canBreakBlock(Block block, ItemStack itemInHand) {
+    return block.isPreferredTool(itemInHand);
   }
 }
